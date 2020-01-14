ab56d5682c8b78a8c703ce5410429932f80b84f6
RaceWalker <damiancierlicki@gmail.com>
2020-01-14 16:52:54 +0100

    Created basic info

---
+++ src/components/Contact.vue
@@ -0,0 +1,59 @@
+<template>
+    <b-container fluid id="contact" class="bg-dark pt-5">
+      <b-row>
+        <b-col
+          v-for="company in companies"
+          :key="company.nip"
+        >
+          <b-jumbotron
+            :header="company.services"
+            :lead="company.company"
+            header-level="4"
+            class="text-left"
+          >
+            <hr class="my-4">
+            <p>
+              Nip: {{ company.nip }} <br>
+              {{ company.street }} <br>
+              {{ company.zip + ' ' + company.city }}
+            </p>
+            <hr class="my-4">
+            <p>
+              Tel: {{ company.phone }} <br>
+              Email: {{ company.email}}
+            </p>
+          </b-jumbotron>
+        </b-col>
+      </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+  name: 'contact',
+  data: () => ({
+    companies: [
+      {
+        company: 'PPUH Agat Krystyna Cierlicka',
+        nip: '1234 1234 1234 1234',
+        street: 'Warneńska 12c/2',
+        zip: '80-288',
+        city: 'Gdańsk',
+        phone: '123 345 789',
+        email: 'uslugibudowlane@gmail.com',
+        services: 'Usługi budowlane'
+      },
+      {
+        company: 'PPUH Abacco Krzysztof Żurek',
+        nip: '1234 1234 1234 1244',
+        street: 'Aleja Grunwaldzka 238d',
+        zip: '80-266',
+        city: 'Gdańsk',
+        phone: '123 345 789',
+        email: 'usługislusarskie@gmail.com',
+        services: 'Usługi ślusarskie'
+      }
+    ]
+  })
+}
+</script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,7 @@
         <nav-bar />
         <information />
         <gallery />
+        <contact />
     </div>
 </template>
 
@@ -10,13 +11,15 @@
 import NavBar from '@/components/NavBar'
 import Information from '@/components/Information'
 import Gallery from '@/components/Gallery'
+import Contact from '@/components/Contact'
 
 export default {
   name: 'Main',
   components: {
     NavBar,
     Information,
-    Gallery
+    Gallery,
+    Contact
   },
   data: () => ({
 
