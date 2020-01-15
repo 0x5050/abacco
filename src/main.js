5e6898dc969489968431f967b2b1c69544719a63
RaceWalker <damiancierlicki@gmail.com>
2020-01-15 22:12:43 +0100

    added firebase config

--- src/main.js
+++ src/main.js
@@ -4,9 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { store } from './store/index'
+import firebase from 'firebase'
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// let app = ''
+
+const config = {
+  apiKey: 'AIzaSyDFen9TsWKSAucNav52E7scL0WYV557pFU',
+  authDomain: 'abacco-e9fd3.firebaseapp.com',
+  databaseURL: 'https://abacco-e9fd3.firebaseio.com',
+  projectID: 'abacco-e9fd3',
+  storageBucket: 'abacco-e9fd3.appspot.com',
+  messagingSenderId: '482185164298'
+}
+
+firebase.initializeApp(config)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
