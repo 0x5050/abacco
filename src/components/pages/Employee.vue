28a0998ffc9dcf47171be8d7977ee6c845e1190f
RaceWalker <damiancierlicki@gmail.com>
2020-01-24 00:56:38 +0100

    Added basic display

--- src/components/pages/Employee.vue
+++ src/components/pages/Employee.vue
@@ -2,18 +2,21 @@
   <div>
     <control-panel>
       <p-e-work-hours />
+      <p-e-calendar-display />
     </control-panel>
   </div>
 </template>
 <script>
 import ControlPanel from '@/components/templates/ControlPanel'
 import PEWorkHours from '@/components/pages/employee_page/WorkHours'
+import PECalendarDisplay from '@/components/pages/employee_page/CalendarDisplay'
 
 export default {
   name: 'P-Employee',
   components: {
     ControlPanel,
-    PEWorkHours
+    PEWorkHours,
+    PECalendarDisplay
   }
 }
 </script>
--- src/components/pages/employee_page/CalendarDisplay.vue
+++ src/components/pages/employee_page/CalendarDisplay.vue
@@ -1,13 +1,42 @@
 <template>
   <div>
-    Calendar
-    {{ uid }}
-    {{ data }}
+    <b-card v-for="item in data" :key="item.date" :title="getMonth(item)" class="text-left" v-b-toggle="item">
+      <b-collapse :id="item">
+        <span v-for="i in item" :key="i.date">
+          <b-row class="mt-2">
+            <b-col lg>
+              <b-input-group prepend="Data">
+                <b-input disabled :value="dateFormat(i.date, 'D')"/>
+              </b-input-group>
+            </b-col>
+            <b-col lg>
+              <b-input-group prepend="Start">
+                <b-input disabled :value="dateFormat(i.start, 'H')"/>
+              </b-input-group>
+            </b-col>
+            <b-col lg>
+              <b-input-group prepend="Stop">
+                <b-input disabled :value="dateFormat(i.stop, 'H')"/>
+              </b-input-group>
+            </b-col>
+            <b-col lg>
+              <b-input-group prepend="Opis">
+                <b-input disabled :value="i.description"/>
+              </b-input-group>
+            </b-col>
+            <b-col lg>
+              Status
+            </b-col>
+          </b-row>
+        </span>
+      </b-collapse>
+    </b-card >
   </div>
 </template>
 
 <script>
 import firebase from 'firebase'
+import { DateTime } from 'luxon'
 
 export default {
   name: 'P-E-Calendar-Display',
@@ -15,6 +44,16 @@ export default {
     uid: '',
     data: []
   }),
+  methods: {
+    getMonth (item) {
+      const firstDate = Object.keys(item)[0]
+      const date = DateTime.fromISO(firstDate)
+      return `${date.month}.${date.year}`
+    },
+    dateFormat (value, type) {
+      return DateTime.fromISO(value).toFormat(type)
+    }
+  },
   async created () {
     await firebase.auth().onAuthStateChanged(user => {
       this.uid = user.uid
