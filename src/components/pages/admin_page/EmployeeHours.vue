e5c7bcbab44e574d80f45a6084e792f3e3a42b89
RaceWalker <damiancierlicki@gmail.com>
2020-01-24 19:55:38 +0100

    basic match uid with email

--- src/components/pages/admin_page/EmployeeHours.vue
+++ src/components/pages/admin_page/EmployeeHours.vue
@@ -1,11 +1,54 @@
 <template>
   <div>
-    Employee Hours
+  <div v-for="hour in result" :key="hour.id">
+    {{ hour.email }} {{ hour.month }} {{ hour.year }} <br>
+  </div>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
-  name: 'P-A-Employee-Hours'
+  name: 'P-A-Employee-Hours',
+  data: () => ({
+    users: [],
+    hours: [],
+    result: []
+  }),
+  async created () {
+    const users = await firebase.firestore().collection('roles').get()
+    users.docs.map(doc => {
+      this.users.push({email: doc.data().email, uid: doc.id})
+    })
+
+    const hours = await firebase.firestore().collection('employee-hours').get()
+    hours.docs.map(doc => {
+      const documentID = doc.id
+      this.hours.push({
+        uid: documentID.slice(0, 28),
+        month: documentID.slice(29, 31),
+        year: documentID.slice(32, 37),
+        data: doc.data(),
+        email: ''
+      })
+    })
+    let i = 0
+    this.users.forEach(user => {
+      this.hours.forEach(hour => {
+        if (hour.uid === user.uid) {
+          this.result.push({
+            email: user.email,
+            uid: user.uid,
+            month: hour.month,
+            year: hour.year,
+            data: hour.data,
+            id: i
+          })
+          i++
+        }
+      })
+    })
+  }
 }
 </script>
