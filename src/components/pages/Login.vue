974bae66404d4ecbe8e2c8ee53cabfacaf62a2ad
RaceWalker <damiancierlicki@gmail.com>
2020-01-24 02:29:01 +0100

    Refactored login page

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,20 +1,65 @@
 <template>
-    <div class="bg-dark h-100 w-100 position-fixed">
-      <b-container class=" text-left text-light center">
-          <b-form @submit="LoginUser">
-            Email
-            <b-input-group md="w-100" prepend="✉️" class="mb-3 mt-1">
-              <b-form-input type="email" v-model="login_form.email"/>
-            </b-input-group>
-            Hasło
-            <b-input-group prepend="🔑" class="mt-1">
-              <b-form-input type="password" v-model="login_form.password"/>
-            </b-input-group>
-            <b-button class="w-25 mt-3" type="submit">Login</b-button>
-          </b-form>
-        </b-container>
-        <b-button @click="RegisterUser">Register</b-button>
-    </div>
+<b-container fluid class="bg-dark h-100 position-fixed">
+  <b-form
+    class="ml-auto mr-auto center"
+    @submit="LoginUser"
+  >
+  <b-row>
+    <b-input-group
+      v-for="input in inputs"
+      :key="input.name"
+      :prepend="input.name"
+      class="mt-3"
+      size="lg"
+    >
+      <b-input
+        v-model="login_form[input.type]"
+        :type="input.type"
+      />
+    </b-input-group>
+  </b-row>
+    <b-row>
+      <b-button
+        class="mt-3 d-block mr-auto"
+        variant="info"
+        size="lg"
+        v-b-modal="'register-modal'"
+      >
+        Zarejestruj
+      </b-button>
+      <b-button
+        class="mt-3 d-block ml-auto"
+        variant="success"
+        type="submit"
+        size="lg"
+      >
+        Zaloguj
+      </b-button>
+    </b-row>
+  </b-form>
+  <b-modal id="register-modal" hide-footer title="Zarejestruj się">
+    <b-input-group
+      v-for="input in inputs"
+      :key="input.name"
+      :prepend="input.name"
+      class="mt-3"
+      size="lg"
+    >
+      <b-input
+        v-model="register_form[input.type]"
+        :type="input.type"
+      />
+    </b-input-group>
+    <b-button
+        class="mt-3 d-block ml-auto"
+        variant="success"
+        size="lg"
+        @click="RegisterUser"
+    >
+        Zarejestruj
+    </b-button>
+  </b-modal>
+</b-container>
 </template>
 
 <script>
@@ -26,7 +71,21 @@ export default {
     login_form: {
       email: '',
       password: ''
-    }
+    },
+    register_form: {
+      email: '',
+      password: ''
+    },
+    inputs: [
+      {
+        name: '✉️',
+        type: 'email'
+      },
+      {
+        name: '🔑',
+        type: 'password'
+      }
+    ]
   }),
   created () {
     firebase.auth().onAuthStateChanged(userAuth => {
@@ -46,8 +105,8 @@ export default {
     },
     RegisterUser () {
       firebase.auth()
-        .createUserWithEmailAndPassword(this.login_form.email, this.login_form.password)
-        .then(console.log('Created User'))
+        .createUserWithEmailAndPassword(this.register_form.email, this.register_form.password)
+        .then(console.log('Utworzono konto'))
     }
   }
 }
