1f9a5efaf1fd42ba056495fc83e119b8ad60fec7
RaceWalker <damiancierlicki@gmail.com>
2020-01-25 23:04:23 +0100

    Added validation to fields and working change password button

--- src/components/pages/Settings.vue
+++ src/components/pages/Settings.vue
@@ -23,9 +23,12 @@
         </b-row>
         <b-row class="mt-2">
           <b-col>
+            {{ old_password }}
             <b-button
               :disabled="!validated"
-              variant="success">
+              variant="success"
+              @click="changePassword()"
+              >
               Zmień
             </b-button>
           </b-col>
@@ -35,8 +38,10 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 import { validationMixin } from 'vuelidate'
-import { required } from 'vuelidate/lib/validators'
+import { required, minLength } from 'vuelidate/lib/validators'
+import { mapMutations } from 'vuex'
 
 export default {
   name: 'P-Settings',
@@ -44,29 +49,52 @@ export default {
   data: () => ({
     fields: [
       {
-        prepend: 'Hasło',
-        value: 'password',
-        error: 'Pole Wymagane'
+        prepend: 'Aktualne Hasło',
+        value: 'old_password',
+        error: 'Minimum 6 znaków'
       },
       {
-        prepend: 'Powtórz hasło',
+        prepend: 'Nowe Hasło',
+        value: 'password',
+        error: 'Minimum 6 znaków'
+      },
+      {
+        prepend: 'Powtórz nowe hasło',
         value: 'retyped_password',
         error: 'Hasła muszą być identyczne'
       }
     ],
+    old_password: null,
     password: null,
     retyped_password: null,
     validated: false
   }),
   validations: {
+    old_password: {
+      required,
+      minLength: minLength(6),
+      validate (value) {
+        return new Promise((resolve, reject) => {
+          if (typeof value === 'string' && value.length >= 6) {
+            this.validated = true
+            resolve(typeof value === 'string' && value.length >= 6)
+          } else {
+            this.validated = false
+            reject(new Error())
+          }
+        })
+      }
+    },
     password: {
-      required
+      required,
+      minLength: minLength(6)
     },
     retyped_password: {
       required,
+      minLength: minLength(6),
       isSame (value) {
         return new Promise((resolve, reject) => {
-          if (value === this.password) {
+          if (value === this.password && value.length >= 6) {
             this.validated = true
             resolve(value === this.password && typeof value === 'string')
           } else {
@@ -78,9 +106,39 @@ export default {
     }
   },
   methods: {
+    ...mapMutations('alert', ['setAlert']),
     validation (name) {
       const { $dirty, $error } = this.$v[name]
       return $dirty ? !$error : null
+    },
+    changePassword () {
+      const user = firebase.auth().currentUser
+      const credential = firebase.auth.EmailAuthProvider.credential(
+        user.email,
+        this.old_password
+      )
+
+      user.reauthenticateWithCredential(credential).then(() => {
+        user.updatePassword(this.password).then(() => {
+          this.setAlert({
+            message: 'Zmieniono Haso',
+            variant: 'success',
+            duration: 2
+          })
+        }).catch(error => {
+          this.setAlert({
+            message: 'Błąd przy zmianie hasła ' + error,
+            variant: 'danger',
+            duration: 2
+          })
+        })
+      }).catch(error => {
+        this.setAlert({
+          message: 'Błąd przy zmianie hasła ' + error,
+          variant: 'danger',
+          duration: 2
+        })
+      })
     }
   }
 }
