a86a5f3c7f7a72f9faacf753cf5fc15e2161ff1d
Damian Cierlicki <damiancierlicki@gmail.com>
2020-01-26 16:23:06 +0100

    refactored funcion getMonth'

--- src/components/templates/EmployeeWorkHours.vue
+++ src/components/templates/EmployeeWorkHours.vue
@@ -131,46 +131,8 @@ export default {
       return DateTime.fromISO(value).toFormat(type)
     },
     getMonth (item) {
-      let month = ''
-      switch (item) {
-        case 1:
-          month = 'Styczeń'
-          break
-        case 2:
-          month = 'Luty'
-          break
-        case 3:
-          month = 'Marzec'
-          break
-        case 4:
-          month = 'Kwiecień'
-          break
-        case 5:
-          month = 'Maj'
-          break
-        case 6:
-          month = 'Czerwiec'
-          break
-        case 7:
-          month = 'Lipiec'
-          break
-        case 8:
-          month = 'Sierpień'
-          break
-        case 9:
-          month = 'Wrzesień'
-          break
-        case 10:
-          month = 'Październik'
-          break
-        case 11:
-          month = 'Listopad'
-          break
-        case 12:
-          month = 'Grudzień'
-          break
-      }
-      return `${month}`
+      const date = DateTime.fromObject({month: item})
+      return date.monthLong.charAt(0).toUpperCase() + date.monthLong.slice(1)
     }
   }
 }
