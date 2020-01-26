0ff8bdc2ffad26d54b6e17fddddfddaccb88b552
Damian Cierlicki <damiancierlicki@gmail.com>
2020-01-26 15:42:57 +0100

    Created invoices template

---
+++ src/components/templates/Invoices.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-container fluid>
+    <h1 class="text-left">
+      Faktury
+    </h1>
+    <b-button
+      v-b-toggle="collapsedId"
+      variant="success"
+      size="lg"
+    >
+      Dodaj Fakturę
+    </b-button>
+    <b-collapse :id="collapsedId">
+      <o-invoices-add />
+    </b-collapse>
+    <o-invoices-display />
+  </b-container>
+</template>
+
+<script>
+import OInvoicesAdd from '@/components/organisms/InvoicesAdd'
+import OInvoicesDisplay from '@/components/organisms/InvoicesDisplay'
+
+export default {
+  name: 'T-Invoices',
+  components: {
+    OInvoicesAdd,
+    OInvoicesDisplay
+  },
+  data: () => ({
+    collapsedId: 'add'
+  })
+}
+</script>
